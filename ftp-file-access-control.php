e0e3724277ab2d532ee2ce0b93dd99479bc8eb26
BeardedGinger <josh@limecuda.com>
2016-03-02 00:00:21 -0500

    plugin header

---
+++ ftp-file-access-control.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name:       FTP File Access Control
+ * Description:       Allows site owner the ability to upload files to a root folder via FTP and users can login and download those files
+ * Version:           1.0.0
+ * Author:            LimeCuda
+ * Author URI:        http://limecuda.com
+ * Text Domain:       ftp-file-access-control
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
